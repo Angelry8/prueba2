5939abbb410a85e945faace88d20287e32cde069
Angelry8 <proangel768@gmail.com>
2025-05-22 21:12:52 -0600

    se agrega un nuevo mensaje en la division

--- division.cpp
+++ division.cpp
@@ -7,6 +7,7 @@ double division(double g, double h){
 int main() {
     double g= 0;
     double h= 0;
+    cout <<"Bienvenido!" <<endl;
     cout <<"Ingrese el primer numero: " <<endl;
     cin >>g;
     cout << "Ingrese el segundo numero: " <<endl;
